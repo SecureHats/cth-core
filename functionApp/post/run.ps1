d4166bbad866b4f72be805f38c479e1f11d3369c
Rogier Dijkman <40334679+azurekid@users.noreply.github.com>
2022-07-26 19:05:56 +0200

    Add files via upload

--- functionApp/post/run.ps1
+++ functionApp/post/run.ps1
@@ -3,7 +3,7 @@ using namespace System.Net
 # Input bindings are passed in via param block.
 param($Request, $TriggerMetadata)
 
-$result =Invoke-Challenge -flagCode "$($Request.Query.code)"
+$result = Invoke-Challenge -flagCode "$($Request.Query.code)"
 
 Push-OutputBinding -Name Response -Clobber -Value ([HttpResponseContext]@{
     StatusCode = [HttpStatusCode]::OK
